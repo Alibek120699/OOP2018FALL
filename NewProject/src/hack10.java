9811ba13b42e72bf5d44f726308a9f2206b3fd7a
Alibek120699 <sayakalibek1@gmail.com>
2018-09-05 12:41:30 +0600

    lab1

--- NewProject/src/hack10.java
+++ NewProject/src/hack10.java
@@ -1,7 +1,35 @@
+import java.math.BigDecimal;
 import java.util.*;
-public class hack10 {
-    public static void main(String args[]){
-        Scanner inp = new Scanner(System.in);
 
+class hack10 {
+    public static void main(String []args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String [] s = new String[n+2];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
+
+
+        Comparator<String> customComparator = new Comparator<String>()
+        {
+
+            public int compare(String s1, String s2) {
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                return b.compareTo(a); // descending order
+            }
+        };
+
+        Arrays.sort(s, 0, n, customComparator);
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
     }
 }
+
+//https://www.hackerrank.com/challenges/java-bigdecimal/problem
